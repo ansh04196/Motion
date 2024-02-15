0b184b6a0b94a258c9aebb1fe20be5e87fe1bcd1
Ansh Chouksey <anshchouksey08@gmail.com>
2024-02-15 15:04:28 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,59 @@
+import { Toaster } from "sonner";
+import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
+
+import { ThemeProvider } from '@/components/provider/theme-provider'
+import { ConvexClientProvider } from '@/components/provider/convex-provider'
+import { ModalProvider } from "@/components/provider/modal-provider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
+
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Motion - Note App',
+  description: 'The connected workspace where better, faster work happens.',
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo-dark.svg",
+        href: "/logo-dark.svg",
+      }
+    ]
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="Motion-theme-2"
+            >
+              <Toaster position="bottom-center" />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
+        </ConvexClientProvider>
+      </body>
+    </html>
+  )
+}
